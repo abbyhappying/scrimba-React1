ac4838452b4ad188c568a96f3c3c67d79942f3a4
Abby <abbyhappying@gmail.com>
2024-10-31 17:55:25 -0700

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (
+      <div className='main'>
+        <main className="main-content">
+          <h1>Reason I'm excited to learn React</h1>
+          <ul>
+            <li>It's a popular Library</li>
+            <li>It's more likely to get a job if I know React</li>
+            <li>Let's start</li>
+          </ul>
+        </main>
+      </div>
+        )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+export default function Nav() {
+  return (
+    <nav>
+        <img src="./logo512.png" width='70px' alt="image"></img>
+        <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Rewards</li>
+        </ul>
+    </nav>
+  )
+}
